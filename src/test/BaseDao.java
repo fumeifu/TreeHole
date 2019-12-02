4c57b25c0620186ff437a5c689ca6e9ae79b5953
fumeifu <43727705+fumeifu@users.noreply.github.com>
2019-12-02 23:10:18 +0800

    解决连接

--- src/test/BaseDao.java
+++ src/test/BaseDao.java
@@ -7,21 +7,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BaseDao {
-	private static final String DRIVER = "com.mysql.jdbc.Driver";
-	//杩炴帴鐨剈rl     localhost鍙互淇敼涓篿p鍦板潃     shopping娉ㄦ剰锛屾槸鏁版嵁搴撳悕锛屼笉鏄繛鎺ュ悕
-	private static final String URL="jdbc:mysql://localhost:3306/treehole";
-	//鐢ㄦ埛鍚�
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	
+	private static final String URL="jdbc:mysql://localhost:3306/treehole?serverTimezone=UTC&characterEncoding=utf-8";
+	
 	private static final String UNAME = "sx";
-	//瀵嗙爜
+	
 	private static final String PWD = "19990128";
-	//connection杩炴帴瀵硅薄     娉ㄦ剰鍖�   鏄�  java.sql.Connection;
+
 	private static Connection con = null;
-	//缁撴灉闆嗗璞�     娉ㄦ剰鍖�   鏄�  java.sql.ResultSet;
+	
 	private static ResultSet rs = null;
-	//棰勭紪璇戝璞�	娉ㄦ剰鍖�   鏄�  java.sql.PreparedStatement;
+	
 	private static PreparedStatement ps = null;
 		
-	//鍔犺浇椹卞姩
+	
 	static{
 		try {
 			Class.forName(DRIVER);
@@ -31,7 +31,7 @@ public class BaseDao {
 		}
 	}
 	
-	//鑾峰彇杩炴帴
+	
 	public static void open(){
 		try {
 			con = DriverManager.getConnection(URL, UNAME, PWD);
@@ -50,13 +50,12 @@ public class BaseDao {
 
 		try {
 			open();
-			//2.3  鎵цSQL锛岃幏寰楃粨鏋滈泦
+			
 			ps = con.prepareStatement(sql);
-			//2.4 缁欏崰浣嶇璧嬪��
+			
 			for(int i=0;i<params.length;i++){
 				ps.setObject(i+1, params[i]);
 			}
-			//2.5 鎵ц
 			count = ps.executeUpdate();
 
 		} catch (Exception e) {
@@ -72,13 +71,10 @@ public class BaseDao {
 
 		try {
 			open();
-			//2.3  鎵цSQL锛岃幏寰楃粨鏋滈泦
 			ps = con.prepareStatement(sql);
-			//2.4 缁欏崰浣嶇璧嬪��
 			for(int i=0;i<params.length;i++){
 				ps.setObject(i+1, params[i]);
 			}
-			//2.5 鎵ц
 			rs = ps.executeQuery();
 
 		} catch (Exception e) {
